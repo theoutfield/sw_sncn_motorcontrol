7cad5bce1c680bd395ad6c65d453ff04a5cf43d6
Andrija Feher <andrija.feher@gmail.com>
2014-08-18 12:11:50 +0200

    Update nodeconfig.h for all apps

--- app_demo_bldc_torque_control/config/node/nodeconfig.h
+++ app_demo_bldc_torque_control/config/node/nodeconfig.h
@@ -1,14 +1,10 @@
 /**
  * @file nodeconfig.h
  * @brief Node config file
- * @author Martin Schwarz <mschwarz@synapticon.com>
  */
 
-#ifndef __NODECONFIG_H__
-#define __NODECONFIG_H__
-
+#pragma once
 
 #define SOMANET_CORE c22
 #define SOMANET_COM null
-#define SOMANET_IFM dc900-rev-a
-#endif
+#define SOMANET_IFM dc100-rev-b
--- app_demo_sin_commutation/config/node/nodeconfig.h
+++ app_demo_sin_commutation/config/node/nodeconfig.h
@@ -1,11 +1,10 @@
 /**
  * @file nodeconfig.h
  * @brief Node config file
- * @author Martin Schwarz <schwarz@synapticon.co>
  */
 
 #pragma once
 
 #define SOMANET_CORE c22
 #define SOMANET_COM null
-#define SOMANET_IFM dc900-rev-a
+#define SOMANET_IFM dc100-rev-b
